e6f08d5650bbbb0c1f87caa1b0a6d2232b5134f3
varshauj <135301635+varshauj@users.noreply.github.com>
2023-09-11 12:41:39 +0530

    Add files via upload
    In Java, i creating here an ATM program for representing ATM transection. In the ATM program, the user has to select an option from the options displayed on the screen. The options are related to withdraw the money, deposit the money, check the balance, and exit.

---
+++ AtmOperationImpl.java
@@ -0,0 +1,58 @@
+package com.edu;
+
+import java.util.HashMap;
+import java.util.Map;
+public   class AtmOperationImpl  implements AtmOperationInterf {
+	ATM atm=new ATM();
+    Map<Double,String> ministmt=new HashMap<>();
+
+
+	@Override
+	public void viewBalance() {
+		// TODO Auto-generated method stub
+		System.out.println("Available Balance is : "+atm.getBalance());
+	
+	}
+
+	@Override
+public void withdrawAmount(double withdrawAmount) {
+		if(withdrawAmount%500==0) {
+		if(withdrawAmount<=atm.getBalance()) {
+                ministmt.put(withdrawAmount, " Amount Withdrawn");
+                System.out.println("Collect the Cash " + withdrawAmount);
+                atm.setBalance(atm.getBalance() - withdrawAmount);
+                viewBalance();
+	}
+
+ else {
+                System.out.println("Insufficient Balance !!");
+            }
+        
+	} else {
+            System.out.println("Please enter the amount in multipal of 500");
+        }
+
+	}
+	
+
+	@Override
+	public void depositAmount(double depositAmount) {
+		// TODO Auto-generated method stub
+		ministmt.put(depositAmount," Amount Deposited");
+        System.out.println(depositAmount+" Deposited Successfully !!");
+        atm.setBalance(atm.getBalance()+depositAmount);
+        viewBalance();
+
+    }	
+	
+
+	@Override
+	public void viewMiniStatement() {
+		// TODO Auto-generated method stub
+		for(Map.Entry<Double,String> m:ministmt.entrySet()){
+            System.out.println(m.getKey()+""+m.getValue());
+        }
+	
+	}
+	}
+	
